c02d3207648280a7a63f2c5de137eaaf561f0dbe
AndreSS <108243074+AndreSS1022@users.noreply.github.com>
2025-03-04 20:07:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const searchCharacter=() =>{
     let character = document.getElementById('search-character').value
-    fetch(`https://rickandmortyapi.com/api/character${id}`)
+    fetch(`https://rickandmortyapi.com/api/character${character }`)
     .then(res => res.json())
     .then(data => displayCharacter(data.data))
 }
@@ -11,22 +11,22 @@ const displayCharacter = characters => {
     characterContainer.innerHTML = ''
     characters.forEach(character => {
         console.log(character)
-        console.log(character.id.image)
+        console.log(character.id)
         const characterDiv = document.createElement("div")
         characterDiv.className = "single-result row align-items-center my-3 p-3"
         characterDiv.innerHTML = `
         
                     <div class="col-lg-2 text-center">
-                        <img class src=${character.id.image} style="height:50px ;" alt=""/>
+                        <img class src=${character.id} style="height:50px ;" alt=""/>
                     </div>
                     <div class="col-lg-4 text-center">
-                        <h3 class="lyrics-name">${character.name}</h3>
-                        <p class="author lead">Album by <span>${character.id.image}</span></p>
+                        <h3 class="character-name">${character.name}</h3>
+                        <p class="character-status">Album by <span>${character.status}</span></p>
                     </div>
                     <div class="col-lg-6 text-center">   
-                        <audio controls>
+                        <speciesCahracter>
                             <source src=${character.species} type="">
-                        </audio>
+                        </speciesCharacter>
                     </div>
                             `
         characterContainer.appendChild(charactergDiv)
