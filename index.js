15ab6238e351abaadb2b8e02b612447e96bd24f1
AndreSS <108243074+AndreSS1022@users.noreply.github.com>
2025-03-04 19:42:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,37 @@
-/* en javascript hay dos formas
- de declarar funciones
 
- 1 //
-function searchSong()
-{
-
-}*/
-
-//2. moderna
-const buscarPersonaje =() =>
-    
-{
-     // se guardar el valor digitado por
-     // el usuario en el html y se guardar
-     // en la variable songName
-
-    let characterName = document.getElementById
-    ('search-character').value
-
-
-    //se envia la variable songName al api
-    // para consultar la informacion de la 
-    // cancion o artista
-
-    
-    fetch(`https://rickandmortyapi.com/api/character
-        ${characterName}`)
-
-        .then(res => res.json())
-        .then(data => displayRickMortyCharacters(data.data))
-
+const searchCharacter=() =>{
+    let character = document.getElementById('search-character').value
+    fetch(`https://rickandmortyapi.com/api/character${id}`)
+    .then(res => res.json())
+    .then(data => displayCharacter(data.data))
 }
 
-
-function displayRickMortyCharacters(characters) {
-    const charactersList = document.getElementById('rickMortyCharacters');
-    charactersList.innerHTML = ''; // Limpiar la lista antes de agregar nuevos personajes
+const displayCharacter = characters => {
+    const characterContainer = document.getElementById("character-container")
+    characterContainer.innerHTML = ''
     characters.forEach(character => {
-        const li = document.createElement('li');
-        li.textContent = `${character.name} - ${character.status} - ${character.species}`;
-        charactersList.appendChild(li);
+        console.log(character)
+        console.log(character.id.image)
+        const characterDiv = document.createElement("div")
+        characterDiv.className = "single-result row align-items-center my-3 p-3"
+        characterDiv.innerHTML = `
+        
+                    <div class="col-lg-2 text-center">
+                        <img class src=${character.id.image} style="height:50px ;" alt=""/>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <h3 class="lyrics-name">${character.name}</h3>
+                        <p class="author lead">Album by <span>${character.id.image}</span></p>
+                    </div>
+                    <div class="col-lg-6 text-center">   
+                        <audio controls>
+                            <source src=${character.species} type="">
+                        </audio>
+                    </div>
+                            `
+        characterContainer.appendChild(charactergDiv)
     });
-
-songDiv.className = "single-result row align-items-center my-3 p-3"
-songDiv.innerHTML = ` 
-
-<div class="col-lg-2 text-center">
-<img class src=${song.album.cover_medium}
- style="height:50px ;" alt=""/>
-                    
-</div>
-
-<div class="col-lg-4 text-center">
- <h3 class="lyrics-name">${song.title}</h3>
-</div>
-
-
+}
 
 `
 
