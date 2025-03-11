b4a4f6b39c13c43f548f5d003b2a48e4c24f2968
AndreSS <108243074+AndreSS1022@users.noreply.github.com>
2025-03-11 13:35:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const searchCharacter=() =>{
-    let character = document.getElementById('search-character').value
-    fetch(`https://rickandmortyapi.com/api/character${character }`)
-    .then(res => res.json())
-    .then(data => displayCharacter(data.data))
+const searchCharacter = () => {
+    let character = document.getElementById('search-character').value;
+    fetch(`https://rickandmortyapi.com/api/character/${character}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.results) {
+                displayCharacter(data.results);
+            } else {
+                console.error("No se encontró el personaje");
+            }
+        })
+        .catch(err => console.error(err));
 }
 
 const displayCharacter = characters => {
-    const characterContainer = document.getElementById("character-container")
-    characterContainer.innerHTML = ''
+    const characterContainer = document.getElementById("character-container");
+    characterContainer.innerHTML = '';
     characters.forEach(character => {
-        console.log(character)
-        console.log(character.id)
-        const characterDiv = document.createElement("div")
-        characterDiv.className = "single-result row align-items-center my-3 p-3"
+        console.log(character);
+        const characterDiv = document.createElement("div");
+        characterDiv.className = "single-result row align-items-center my-3 p-3";
         characterDiv.innerHTML = `
-        
-                    <div class="col-lg-2 text-center">
-                        <img class src=${character.id} style="height:50px ;" alt=""/>
-                    </div>
-                    <div class="col-lg-4 text-center">
-                        <h3 class="character-name">${character.name}</h3>
-                        <p class="character-status">Album by <span>${character.status}</span></p>
-                    </div>
-                    <div class="col-lg-6 text-center">   
-                        <speciesCahracter>
-                            <source src=${character.species} type="">
-                        </speciesCharacter>
-                    </div>
-                            `
-        characterContainer.appendChild(charactergDiv)
+            <div class="col-lg-2 text-center">
+                <img src="${character.image}" style="height:50px;" alt=""/>
+            </div>
+            <div class="col-lg-4 text-center">
+                <h3 class="character-name">${character.name}</h3>
+                <p class="character-status">Status: <span>${character.status}</span></p>
+            </div>
+            <div class="col-lg-6 text-center">   
+                <div>Species: <span>${character.species}</span></div>
+            </div>
+        `;
+        characterContainer.appendChild(characterDiv);
     });
 }
-
-
-
